7e3f604429b78e50835ec09d28f1510139d3e867
rails udemy course <syeda.sara@welldev.io>
2023-08-17 15:36:46 +0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
+class EmailFormatValidator < ActiveModel::EachValidator
+ def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
     has_many :leave_requests
     has_many :team_members
     has_many :teams, through: :team_members
     
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :email, uniqueness: true, email_format: true
+    validates :password, presence: true, length: { minimum: 6,
+               message: " must be minimum %{count} charecters"}, on: :create
 
     has_secure_password
 end
